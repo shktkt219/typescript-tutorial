c725839e47572633ec5cf4e52b8d3b800af42492
shktkt219 <shktkt219@gmail.com>
2023-11-10 11:25:21 +0100

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('mario');
+// let greet = () => {
+//   console.log('hello world');
+// }
+let greet;
+// greet = 'hello';
+greet = () => {
+    console.log('hello, again');
+};
+greet();
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    console.log(a + b);
+};
+minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,26 @@
-console.log('mario');
+// let greet = () => {
+//   console.log('hello world');
+// }
+
+let greet: Function;
+
+// greet = 'hello';
+
+greet = () => {
+  console.log('hello, again')
+}
+
+greet();
+
+const add = (a: number, b: number, c:number|string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number): number => {
+  console.log(a + b);
+}
+
+minus(10, 7);
