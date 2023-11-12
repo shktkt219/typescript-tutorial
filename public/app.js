2a6fa4989506e019df1ec4b88f7eb5685f82f638
shktkt219 <shktkt219@gmail.com>
2023-11-12 16:34:40 +0100

    Modules

--- public/app.js
+++ public/app.js
@@ -1,18 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
@@ -22,7 +8,6 @@ invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
 });
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
