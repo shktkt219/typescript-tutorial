c3c534e028436d537f4546b0381ee6e0a27f53b6
shktkt219 <shktkt219@gmail.com>
2023-11-12 15:51:40 +0100

    Classes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,23 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// // if (anchor) {
-// //   console.log(anchor.href);
-// // }
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
